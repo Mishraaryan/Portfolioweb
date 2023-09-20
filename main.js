5226e21f56519e2280e5009b6fa97a1283b333d8
Aryan mishra <118338974+Mishraaryan@users.noreply.github.com>
2023-09-20 06:43:00 +0000

    changed

--- main.js
+++ main.js
@@ -37,3 +37,12 @@ var loader = document.getElementById("preloader");
 window.addEventListener("load", function(){
     loader.style.display = "none"
 })
+
+const toTop = document.querySelector(".to-top");
+window.addEventListener("scroll", () =>{
+    if(window.pageYOffset > 200){
+        toTop.classList.add("active");
+    }else{
+        toTop.classList.remove("active");
+    }
+})
